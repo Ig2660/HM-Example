36f17e85da085bd7396609891b3262663a04791d
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2022-12-03 23:23:51 +0300

    Семинар 2

---
+++ Sem2/Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+while (number >= 999 || number <= -999)
+{
+    number = number / 10;
+}
+
+if (number < 0)
+{
+    number = number * (-1);
+}
+
+if (number >= 100)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Sem2/Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number >= 1 && number <=5)
+{
+    Console.WriteLine("нет");
+}
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("да");
+}
