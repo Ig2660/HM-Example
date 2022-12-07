10dc76ab8ace330e16124851f8b4536e256993d7
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2022-12-07 12:26:50 +0300

    Семинар 3

---
+++ Sem3/Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Является ли введенное число палиндромом");
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 10 == number / 10000 && number / 10 % 10 == number / 1000 % 10)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
---
+++ Sem3/Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 0)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+else
+{
+    for (int i = -1; i >= number; i--)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
