dd7db7e374a0627d9efdcd83d7122361505703b7
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2022-12-21 07:00:41 +0300

    Семинар 6

---
+++ Sem6/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter amount numbers");
+
+int numberCount = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(CountPositiveNumbersFromConsole(numberCount));
+
+int CountPositiveNumbersFromConsole(int numberCount)
+{
+    int counter = 0;
+    for (int i = 0; i < numberCount; i++)
+    {
+        int currentNumber = int.Parse(Console.ReadLine()!);
+        if (IsPositive(currentNumber))
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+bool IsPositive(int number)
+{
+    if (number > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    //return number > 0;
+}
---
+++ Sem6/Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter value b1 ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter value b2 ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter value k1 ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter value k2 ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double[] intersectPoiht = GetIntersectPoint(b1,b2,k1,k2);
+
+Console.WriteLine(intersectPoiht[0]);
+Console.WriteLine(intersectPoiht[1]);
+
+double[] GetIntersectPoint(double b1, double b2, double k1, double k2)
+{
+    double[] result = new double[2];
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x - b2;
+
+    result[0] = x;
+    result[1] = y;
+
+    return result;
+}
