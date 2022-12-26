b8acfacc19dbacc24118368da6386c2d8d4c9954
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2022-12-26 21:40:16 +0300

    Семинар 8

---
+++ Sem8/Task54/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Enter m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matrix = Create2DArray(m, n, 1, 10);
+Print2DArray(matrix);
+Console.WriteLine();
+SortsElementArray(matrix);
+Print2DArray(matrix);
+
+int[,] SortsElementArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Sem8/Task56/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] matrix = Create2DArray(4, 4, 1, 10);
+Print2DArray(matrix);
+Console.WriteLine();
+
+Console.WriteLine(SmallSumString(matrix));
+
+int SmallSumString(int[,] matrix)
+{
+    int[] sumString = new int[matrix.GetLength(0)];
+    int min = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumString[i] += matrix[i,j];
+            if (i == 0)
+            {
+                min = sumString[i];
+            }
+        }
+            if (sumString[i] < min)
+            {
+                min = sumString[i];
+            }
+    }
+    return min;
+}
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
